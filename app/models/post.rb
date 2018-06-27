bc2976797c528a42c09f07d218fdcb8d04adda41
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-06-27 00:58:57 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :title, presence: true
+  validates :size, inclusion: { in: %w(Fiction Non-Fiction)}
+
 end
